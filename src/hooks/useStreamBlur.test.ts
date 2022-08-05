1e8144c0762558f67266df492f6f38b99804f2b6
kmoyse <kevin.moyse@apizee.com>
2022-08-05 17:26:44 +0200

    enhance useStreamBlur tests

--- src/hooks/useStreamBlur.test.ts
+++ src/hooks/useStreamBlur.test.ts
@@ -17,6 +17,7 @@ jest.mock('@apirtc/apirtc', () => {
         ...originalModule,
         Stream: jest.fn().mockImplementation((data: MediaStream | null, opts: any) => {
             return {
+                releaseCalled: false,
                 getId: () => { return 'Mock' },
                 blur: () => {
                     return new Promise<any>((resolve, reject) => {
@@ -24,13 +25,14 @@ jest.mock('@apirtc/apirtc', () => {
                             reject('fail')
                         } else {
                             resolve({
+                                releaseCalled: false,
                                 getId: () => { return 'Blurred' },
-                                release: () => { }
+                                release: function () { this.releaseCalled = true }
                             })
                         }
                     })
                 },
-                release: () => { }
+                release: function () { this.releaseCalled = true }
             }
         }),
     }
@@ -46,41 +48,63 @@ describe('useStreamBlur', () => {
         act(() => {
             result.current.toggle()
         })
+
+        //await waitForNextUpdate()
+
         expect(result.current.stream).toBe(undefined)
+        expect(result.current.blurred).toBe(false)
     })
 
-    test(`initialBlur false`, () => {
+    test(`undefined stream, initialBlur false`, () => {
         let initialBlur: boolean = false
         const { result } = renderHook(() => useStreamBlur(undefined, initialBlur))
         expect(result.current.stream).toBe(undefined)
         expect(result.current.blurred).toBe(false)
     })
 
-    test(`initialBlur true`, () => {
+    test(`undefined stream, initialBlur true`, () => {
         let initialBlur: boolean = true
         const { result } = renderHook(() => useStreamBlur(undefined, initialBlur))
         expect(result.current.stream).toBe(undefined)
         expect(result.current.blurred).toBe(false)
     })
 
-    test(`With a Stream, no blur by default`, () => {
+    test(`With a Stream, not to be blurred`, () => {
         let initialBlur: boolean = false
         const initStream = new Stream(null, {})
         const { result } = renderHook(() => useStreamBlur(initStream, initialBlur))
         expect(result.current.stream?.getId()).toBe('Mock')
         expect(result.current.blurred).toBe(false)
+
+        expect((result.current.stream as any).releaseCalled).toBe(false)
     })
 
-    test(`With a Stream, blurred by default`, async () => {
+    test(`With a Stream, to be blurred`, async () => {
         let initialBlur: boolean = true
         const initStream = new Stream(null, {})
         const { result, waitForNextUpdate } = renderHook(() => useStreamBlur(initStream, initialBlur))
         await waitForNextUpdate()
-        expect(result.current.stream?.getId()).toBe('Blurred')
+
+        expect(result.current.stream).toBeDefined()
+        expect(result.current.stream).not.toBe(initStream)
+
+        const blurredStream = result.current.stream
+        expect(blurredStream?.getId()).toBe('Blurred')
+        expect((blurredStream as any).releaseCalled).toBe(false)
         expect(result.current.blurred).toBe(true)
+
+        // toggle (unblur)
+        act(() => {
+            result.current.toggle()
+        })
+
+        // the blurred stream shall be released
+        expect((blurredStream as any).releaseCalled).toBe(true)
+        // the out stream shall now be the initial stream
+        expect(result.current.stream).toBe(initStream)
     })
 
-    test(`With a Stream, blurred by default, blur fails`, async () => {
+    test(`With a Stream, to be blurred, blur fails`, async () => {
         let initialBlur: boolean = true
         const initStream = new Stream(null, { fail: true })
         const { result, waitForNextUpdate } = renderHook(() => useStreamBlur(initStream, initialBlur))

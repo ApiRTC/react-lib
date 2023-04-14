6005a0fee930572492e4bbf38e704d214237a962
kmoyse <kevin.moyse@apizee.com>
2023-04-14 17:12:38 +0200

    useUserMediaDevices: manage selection and localStorage

--- src/hooks/useUserMediaDevices.spec.ts
+++ src/hooks/useUserMediaDevices.spec.ts
@@ -2,7 +2,7 @@ import { act, renderHook } from '@testing-library/react-hooks';
 
 import './getDisplayMedia.mock';
 
-import { Session, UserAgent, UserAgentOptions } from '@apirtc/apirtc';
+import { Session, UserAgent, UserAgentOptions, MediaDevice } from '@apirtc/apirtc';
 
 // Partial mocking @apirtc/apirtc module
 // see https://jestjs.io/docs/mock-functions
@@ -23,10 +23,14 @@ jest.mock('@apirtc/apirtc', () => {
                 },
                 removeListener: (event: string, fn: Function) => { },
                 getUserMediaDevices: () => {
+                    const audio01 = new MediaDevice('idA01', 'audio', 'label');
+                    const audio02 = new MediaDevice('idA02', 'audio', 'label');
+                    const audio03 = new MediaDevice('idA03', 'audio', 'label');
+                    const video01 = new MediaDevice('idV01', 'video', 'label');
                     return {
-                        audioinput: { 'idA01': { id: 'idA01' }, 'idA02': { id: 'idA02' } },
-                        audiooutput: { 'idA03': { id: 'idA03' } },
-                        videoinput: { 'idV01': { id: 'idV01' } }
+                        audioinput: { 'idA01': audio01, 'idA02': audio02 },
+                        audiooutput: { 'idA03': audio03 },
+                        videoinput: { 'idV01': video01 }
                     }
                 },
                 simulateMediaDeviceChanged: () => {
@@ -39,6 +43,11 @@ jest.mock('@apirtc/apirtc', () => {
                 getUserAgent: () => { return ua }
             }
         }),
+        MediaDevice: jest.fn().mockImplementation((id: string, type: string, label: string) => {
+            return {
+                getId: () => { return id }
+            }
+        })
     }
 })
 
@@ -69,11 +78,49 @@ describe('useUserMediaDevices', () => {
             (userAgent as any).simulateMediaDeviceChanged()
         })
 
-        expect(result.current.userMediaDevices).toStrictEqual(
-            {
-                audioinput: { 'idA01': { id: 'idA01' }, 'idA02': { id: 'idA02' } },
-                audiooutput: { 'idA03': { id: 'idA03' } },
-                videoinput: { 'idV01': { id: 'idV01' } }
-            });
+        // expect(result.current.userMediaDevices).toStrictEqual(
+        //     {
+        //         audioinput: { 'idA01': { id: 'idA01' }, 'idA02': { id: 'idA02' } },
+        //         audiooutput: { 'idA03': { id: 'idA03' } },
+        //         videoinput: { 'idV01': { id: 'idV01' } }
+        //     });
+
+        expect(result.current.userMediaDevices.audioinput['idA01']).toBeDefined()
+        expect(result.current.userMediaDevices.audioinput['idA01'].getId()).toBe('idA01')
+
+        expect(result.current.userMediaDevices.audioinput['idA02']).toBeDefined()
+        expect(result.current.userMediaDevices.audiooutput['idA03']).toBeDefined()
+        expect(result.current.userMediaDevices.videoinput['idV01']).toBeDefined()
+
+        expect(result.current.selectedAudioIn).toBeUndefined()
+        expect(result.current.selectedAudioOut).toBeUndefined()
+        expect(result.current.selectedVideoIn).toBeUndefined()
     })
+
+    test(`localStorage`, () => {
+
+        const userAgent = new UserAgent({});
+        const session = new Session(userAgent, {});
+
+        localStorage.setItem('apirtc' + 'audioInputId', 'idA02')
+        localStorage.setItem('apirtc' + 'audioOutputId', 'idA03')
+        localStorage.setItem('apirtc' + 'videoInputId', 'idV01')
+
+        const { result } = renderHook(() => useUserMediaDevices(session));
+        expect(result.current.userMediaDevices).toStrictEqual({ audioinput: {}, audiooutput: {}, videoinput: {} });
+
+        act(() => {
+            (userAgent as any).simulateMediaDeviceChanged()
+        })
+
+        expect(result.current.selectedAudioIn).toBeDefined()
+        expect(result.current.selectedAudioIn?.getId()).toBe('idA02')
+
+        expect(result.current.selectedAudioOut).toBeDefined()
+        expect(result.current.selectedAudioOut?.getId()).toBe('idA03')
+
+        expect(result.current.selectedVideoIn).toBeDefined()
+        expect(result.current.selectedVideoIn?.getId()).toBe('idV01')
+    })
+
 })
--- src/hooks/useUserMediaDevices.ts
+++ src/hooks/useUserMediaDevices.ts
@@ -1,13 +1,22 @@
-import { MediaDeviceList, Session, UserAgent } from '@apirtc/apirtc';
+import { MediaDevice, MediaDeviceList, Session, UserAgent } from '@apirtc/apirtc';
 import { useEffect, useState } from 'react';
 
-const EMPTY_LIST: MediaDeviceList = { audioinput: {}, audiooutput: {}, videoinput: {} }
+const EMPTY_LIST: MediaDeviceList = { audioinput: {}, audiooutput: {}, videoinput: {} };
 
 const HOOK_NAME = "useUserMediaDevices";
 export default function useUserMediaDevices(
-    session: Session | undefined
+    session: Session | undefined,
+    storageKeyPrefix: string = "apirtc"
 ) {
     const [userMediaDevices, setUserMediaDevices] = useState<MediaDeviceList>(EMPTY_LIST);
+
+    const [selectedAudioIn, setSelectedAudioIn] = useState<MediaDevice>();
+    const [selectedAudioOut, setSelectedAudioOut] = useState<MediaDevice>();
+    const [selectedVideoIn, setSelectedVideoIn] = useState<MediaDevice>();
+
+    const AUDIO_INPUT_ID_KEY = storageKeyPrefix + 'audioInputId';
+    const AUDIO_OUTPUT_ID_KEY = storageKeyPrefix + 'audioOutputId';
+    const VIDEO_INPUT_ID_KEY = storageKeyPrefix + 'videoInputId';
 
     useEffect(() => {
         if (session) {
@@ -29,7 +38,57 @@ export default function useUserMediaDevices(
         }
     }, [session])
 
+    useEffect(() => {
+        const audioInputId = localStorage.getItem(AUDIO_INPUT_ID_KEY);
+        const audioOutputId = localStorage.getItem(AUDIO_OUTPUT_ID_KEY);
+        const videoInputId = localStorage.getItem(VIDEO_INPUT_ID_KEY);
+
+        if (globalThis.apirtcReactLibLogLevel.isDebugEnabled) {
+            console.debug(HOOK_NAME + '|userMediaDevices, audioInputId, audioOutputId, videoInputId', userMediaDevices, audioInputId, audioOutputId, videoInputId)
+        }
+
+        if (audioInputId && userMediaDevices.audioinput[audioInputId]) {
+            setSelectedAudioIn(userMediaDevices.audioinput[audioInputId])
+        }
+        if (audioOutputId && userMediaDevices.audiooutput[audioOutputId]) {
+            setSelectedAudioOut(userMediaDevices.audiooutput[audioOutputId])
+        }
+        if (videoInputId && userMediaDevices.videoinput[videoInputId]) {
+            setSelectedVideoIn(userMediaDevices.videoinput[videoInputId])
+        }
+    }, [userMediaDevices])
+
+    useEffect(() => {
+        if (selectedAudioIn) {
+            if (globalThis.apirtcReactLibLogLevel.isDebugEnabled) {
+                console.debug(HOOK_NAME + '|Storing audioInputId', selectedAudioIn.getId())
+            }
+            localStorage.setItem(AUDIO_INPUT_ID_KEY, selectedAudioIn.getId())
+        }
+    }, [selectedAudioIn])
+
+    useEffect(() => {
+        if (selectedAudioOut) {
+            if (globalThis.apirtcReactLibLogLevel.isDebugEnabled) {
+                console.debug(HOOK_NAME + '|Storing audioOutputId', selectedAudioOut.getId())
+            }
+            localStorage.setItem(AUDIO_OUTPUT_ID_KEY, selectedAudioOut.getId())
+        }
+    }, [selectedAudioOut])
+
+    useEffect(() => {
+        if (selectedVideoIn) {
+            if (globalThis.apirtcReactLibLogLevel.isDebugEnabled) {
+                console.debug(HOOK_NAME + '|Storing videoInputId', selectedVideoIn.getId())
+            }
+            localStorage.setItem(VIDEO_INPUT_ID_KEY, selectedVideoIn.getId())
+        }
+    }, [selectedVideoIn])
+
     return {
-        userMediaDevices
+        userMediaDevices,
+        selectedAudioIn, setSelectedAudioIn,
+        selectedAudioOut, setSelectedAudioOut,
+        selectedVideoIn, setSelectedVideoIn
     }
 }
